42a2de0abe24550edde242ca3f7be71b829eeb3b
SOLEDAD <SOLEDAD@DESKTOP-J9P61QP>
2022-07-03 15:24:14 -0300

    validacion de empleador y vista de registro confirmado

--- TPFinal-Grupo11/src/main/java/ar/edu/unju/fi/controller/EmpleadorController.java
+++ TPFinal-Grupo11/src/main/java/ar/edu/unju/fi/controller/EmpleadorController.java
@@ -2,14 +2,20 @@ package ar.edu.unju.fi.controller;
 
 import javax.validation.Valid;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.unju.fi.entity.Empleador;
+
 import ar.edu.unju.fi.service.IEmpleadorService;
 
 @Controller
@@ -27,8 +33,15 @@ public class EmpleadorController {
 	}
 	
 	@PostMapping("/saveEmpleador")
-	public String guardar(@Valid Empleador empleador, Model model) {
+	public String guardar(@Valid Empleador empleador, BindingResult bindingResult ) {
+		if(bindingResult.hasErrors()) {
+			return "nuevo_empleador";
+		
+		}
+	
 		empleadorService.guardarEmpleador(empleador);
-		return "redirect:/inicio";
+		    return "registro_confirmado";
 	}
+	
+	
 }
